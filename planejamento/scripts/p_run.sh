c3ac14d21a2305f3823f0ad5b330b761aa919aa0
danielb <danibrigante7@gmail.com>
2022-05-06 04:07:01 -0300

    Add files via upload

---
+++ planejamento/scripts/p_run.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+rostopic echo -n 1 /clock > t_inicio_total
+
+printf "\nIniciando Leitor QR Code\n"
+xterm -e "roslaunch leitor_qr leitor_qr.launch" &
+
+printf "\nLista de pontos - Entrada\n"
+cat pontos.csv
+
+printf "\nRodando o TSP\n"
+python3 cvDict.py
+ 
+printf "\nLista de pontos - Saida\n"
+cat pontos_o.csv
+ 
+file="pontos_o.csv"
+while IFS= read -r line
+do
+	rostopic echo -n 1 /clock > t_inicio
+	python3 a_star.py $line
+	python3 sampleMove.py
+	sleep 3
+	rosrun scan_prateleira scan_prateleira_node
+	rostopic echo -n 1 /clock > t_fim
+	printf "\nTempo incial:\n"
+	cat t_inicio
+	printf "\nTempo final:\n"
+	cat t_fim
+done < "$file"
+ 
+rostopic echo -n 1 /clock > t_final_total
+
+printf "\nFim do Programa...\n"
+ 
+printf "\nTempo incial total:\n"
+cat t_inicio_total
+ 
+printf "\nTempo final total:\n"
+cat t_final_total                                                                                                                                                                         
